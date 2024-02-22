20d4f2bf2f950f17abcc53d916f898412e06f1ba
gabrielgcosta <gabriel.costa@codenapp.com>
2024-02-22 12:29:03 -0300

    first commit

---
+++ src/main/java/com/gabrielgcosta/apitransaction/ApitransactionApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielgcosta.apitransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitransactionApplication.class, args);
+	}
+
+}
